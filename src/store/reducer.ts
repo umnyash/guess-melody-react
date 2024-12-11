ee71eadc2042b62de01f8539392e76c8a8815284
Rustam Sadreev <rustam17.sb@gmail.com>
2024-12-11 03:19:43 +0300

    feat: add actions and reducer

---
+++ src/store/reducer.ts
@@ -0,0 +1,23 @@
+import { createReducer } from '@reduxjs/toolkit';
+import { incrementStep, resetGame } from './action';
+import { FIRST_GAME_STEP } from '../const';
+
+const STEP_COUNT = 1;
+
+const initialState = {
+  mistakes: 0,
+  step: FIRST_GAME_STEP,
+};
+
+const reducer = createReducer(initialState, (builder) => {
+  builder
+    .addCase(incrementStep, (state) => {
+      state.step = state.step + STEP_COUNT;
+    })
+    .addCase(resetGame, (state) => {
+      state.mistakes = 0;
+      state.step = FIRST_GAME_STEP;
+    });
+});
+
+export { reducer };
