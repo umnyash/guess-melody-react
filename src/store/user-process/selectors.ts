c613ebbce4088b8ca769326d2d704bbc6a450e88
Rustam Sadreev <rustam17.sb@gmail.com>
2025-03-27 15:41:10 +0300

    refactor: narrow down the state type in selectors

--- src/store/user-process/selectors.ts
+++ src/store/user-process/selectors.ts
@@ -2,8 +2,8 @@ import { NameSpace } from '../../const';
 import { State } from '../../types/state';
 import { AuthorizationStatus } from '../../const';
 
-export const getAuthorizationStatus = (state: State): AuthorizationStatus =>
+export const getAuthorizationStatus = (state: Pick<State, NameSpace.User>): AuthorizationStatus =>
   state[NameSpace.User].authorizationStatus;
 
-export const getAuthCheckedStatus = (state: State): boolean =>
+export const getAuthCheckedStatus = (state: Pick<State, NameSpace.User>): boolean =>
   state[NameSpace.User].authorizationStatus !== AuthorizationStatus.Unknown;
